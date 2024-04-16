be121eba03a0e1ca370a4aaede84bfb8b5ad2fc4
Huzaifa Ali Khan <108281380+Huzaifaak1@users.noreply.github.com>
2024-04-16 21:50:04 +0500

    Add files via upload
    16th April Session

---
+++ 16_4.py
@@ -0,0 +1,29 @@
+
+
+
+# myStr = "Hello" # H,o
+# lengthOfString = len(myStr)
+# print(myStr[0],myStr[lengthOfString-1])
+
+
+# fullName = input("Enter your full name: ") # Huzaifa Ali Khan
+# nameList = fullName.split(' ') # ['Huzaifa', 'Ali', 'Khan']
+# firstName = nameList[0]
+# lastName = nameList[len(nameList)-1]
+# print(f"Mr. {lastName}")
+
+
+
+# myStr = "Hello how are you"
+# myList = myStr.split(' ') # ["Hello","how","are","you"]
+# print(myList)
+
+
+
+
+# student1 = "Huzaifa"
+# student2 = "Kiran"
+# student3 = "Haris"
+    # Alternate using 'List' below
+# students = ["Huzaifa","Kiran","Haris","Adyan","Sam"] # list
+# print(students)
